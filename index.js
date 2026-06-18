879f7be6a5cb8d614d59572ffc1cf95230171ccf
Katreenaaa <katrinaemegbagha@gmail.com>
2026-06-18 18:19:00 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/* eslint-env node */
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
