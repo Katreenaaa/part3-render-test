411d34911ad45dc0af71ac1cdc272de264aa3841
Katreenaaa <katrinaemegbagha@gmail.com>
2026-06-19 13:21:32 +0100

    Serve frontend from Express backend

--- index.js
+++ index.js
@@ -6,6 +6,29 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static("dist"));
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
